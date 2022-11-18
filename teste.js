c6b710f7c7fdc92014d1629f0851b03462d7d85e
Leonardo Santos Silva <1532leleo@gmail.com>
2022-11-18 20:51:45 -0300

    Lista 28 Incompleta 100%

--- teste.js
+++ teste.js
@@ -1,8 +1,8 @@
 // Parte 1
+
 // Crie as seguintes classes: Imóvel e Pessoa;
 
-// Um Imóvel deve ter os seguintes atributos: endereço, cep, area em metros, quantidade de
-// banheiros, quantidade de quartos, as pessoas que moram neste imóvel e o valor do
+// Um Imóvel deve ter os seguintes atributos: endereço, cep, area em metros, quantidade de banheiros, quantidade de quartos, as pessoas que moram neste imóvel e o valor do
 // mesmo;
 
 // Uma pessoa deve ter os seguintes atributos: nome, cpf e uma data de nascimento;
@@ -17,13 +17,13 @@
 
 // Uma Kitnet pode ter até dois banheiros, um apartamento pode ter três banheiros e uma casa pode ter até cinco banheiros;
 
+// Cada tipo de Imóvel deve ter um método para calcular seu valor com base nas seguintes especificações:
 
-// Cada tipo de Imóvel deve ter um método para calcular seu valor com base nas seguintes
-// especificações:
 // Uma casa com quintal tem um acréscimo de 30% no seu valor;
-// Um apartamento deve ter o valor do seu condomínio somado ao valor base;
-// Uma Kitnet não tem alterações no seu valor base;
 
+// Um apartamento deve ter o valor do seu condomínio somado ao valor base;
+
+// Uma Kitnet não tem alterações no seu valor base;
 
 class Imovel {
     Endereco
@@ -34,23 +34,24 @@ class Imovel {
     Morador
     ValorImovel
 
-    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel) {
+    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel) {
         this.Endereco = endereco
         this.Cep = cep
         this.AreaEmMetros = areaEmMetros
         this.QtdeBanheiros = qtdeBanheiros
         this.QtdeQuartos = qtdeQuartos
-        this.Morador = morador
         this.ValorImovel = valorImovel
     }
 
     AdicionarMorador(pessoa) {
-        this.Morador = Pessoa
-
-        
+        this.Morador = pessoa
+        console.log("Morador: " + this.Morador.Nome)
+        console.log("CPF:" + this.Morador.Cpf)
+        console.log("Data de Nascimento:" + this.Morador.DataDeNascimento)
     }
-    RemoverMorador(pessoa){
-
+    RemoverMorador(){
+        this.Morador = null
+        console.log("O morador se mudou!")
     }
 }
 class Pessoa {
@@ -67,23 +68,49 @@ class Pessoa {
 
 class Casa extends Imovel {
     ComQuintal
-    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel, comQuintal) {
-        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel)
+    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel, comQuintal) {
+        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel)
         this.ComQuintal = comQuintal
+    }
+
+    AcrescimoQuintal(){
+        let valorTotal = 0
+        if(this.ComQuintal == true){
+            valorTotal = this.ValorImovel + (this.ValorImovel * 0.3)
+        }else{
+            valorTotal = this.ValorImovel
+        }
+        console.log("O valor total da Casa com o acréscimo é:" + valorTotal)
+        
     }
 }
 class Apartamento extends Imovel {
     Andar
     ValorCondominio
-    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel, andar, valorCondominio) {
-        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel)
+    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel, andar, valorCondominio) {
+        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel)
         this.Andar = andar
         this.ValorCondominio = valorCondominio
     }
+    ValorTotalAp(){
+        let valorTotal1 = this.ValorImovel + this.ValorCondominio
+        return valorTotal1
+    }
+    
 }
 class Kitnet extends Imovel {
-    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel) {
-        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, morador, valorImovel)
+    constructor(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel) {
+        super(endereco, cep, areaEmMetros, qtdeBanheiros, qtdeQuartos, valorImovel)
 
     }
 }
+
+let Pessoa1 = new Pessoa("Juliano", "628392773-89", "25/09/2002")
+
+let Casa1 = new Casa("Rua Rio de Janeiro", "08295-410", 800, 4, 3,  350000, true)
+
+let Ap1 = new Apartamento("Rua Pitanga", "36787-367", 80, 3, 2, 450000, 12, 800)
+
+let Kitnet1 = new Kitnet( "Rua Severo", "06734-152", 25, 1, 1, 85000)
+
+
